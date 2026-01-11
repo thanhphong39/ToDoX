70eaf47be54b0a3349c7aa4f0163b617784efaf9
thanhphong39 <phongntse181679@fpt.edu.vn>
2026-01-11 21:46:09 +0700

    Create componet

--- frontend/project/src/pages/HomePage.jsx
+++ frontend/project/src/pages/HomePage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import AddTask from "@/components/AddTask";
+import Header from "@/components/Header";
+import StatsAndFilters from "@/components/StatsAndFilters";
+import TaskList from "@/components/TaskList";
+import TaskListPagination from "@/components/TaskListPagination";
+import DateTimeFilter from "@/components/DateTimeFilter";
+import Footer from "@/components/Footer";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="min-h-screen w-full bg-[#fefcff] relative">
+      {/* Dreamy Sky Pink Glow */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+        radial-gradient(circle at 30% 70%, rgba(173, 216, 230, 0.35), transparent 60%),
+        radial-gradient(circle at 70% 30%, rgba(255, 182, 193, 0.4), transparent 60%)`,
+        }}
+      />
+      {/* Your Content/Components */}
+      <div className="container pt-8 mx-auto relative z-10">
+        <div className="w-full max-w-2xl -6 mx-auto space-y-6">
+          <Header />
 
-export default HomePage
+          <AddTask />
+
+          <StatsAndFilters />
+
+          <TaskList />
+
+          <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <TaskListPagination />
+            <DateTimeFilter />
+          </div>
+
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/project/src/pages/NotFound.jsx
+++ frontend/project/src/pages/NotFound.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className= "flex flex-col items-center justify-center min-h-screen bg-gray-50">
+       <img
+        src="404_NotFound.png"
+        alt="not found"   
+        className="w-1/2 max-w-md mb-8"
+      />
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">404 - Page Not Found</h1>
+      <p className="text-lg text-gray-600 mb-8">  Sorry, the page you are looking for does not exist. </p>
+      <a
+        href="/"
+        className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+      >
+        Trở về trang chủ
+      </a>
+    </div>
   )
 }
 
