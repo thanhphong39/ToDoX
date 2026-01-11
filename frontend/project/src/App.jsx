8b5f6b93372c9839b19ed42a1b936bf7b98eaee1
thanhphong39 <phongntse181679@fpt.edu.vn>
2026-01-11 12:10:23 +0700

    Setup Frontend

--- frontend/project/src/App.jsx
+++ frontend/project/src/App.jsx
@@ -1,12 +1,22 @@
-
-import './App.css'
+import React from 'react'
+import {Toaster} from 'sonner'
+import {BrowserRouter,Routes,Route} from 'react-router'
+import NotFound from './pages/NotFound.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 function App() {
  
 
   return (
     <>
-      
+    <Toaster position='top-right' richColors />
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
